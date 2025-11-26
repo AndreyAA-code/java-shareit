693f8d2021f433fc4438a7c859fe114f83e0d2d1
AndreyAA-code <otd53@yandex.ru>
2025-11-27 00:23:05 +0300

    15th sprint task accomplished

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(classes = ShareItApp.class)
 @ActiveProfiles("test")
 class ShareItTests {
 
