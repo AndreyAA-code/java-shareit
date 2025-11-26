6a7cdb6fd7da9e5b55dc2c53bfd053f5046157e1
AndreyAA-code <otd53@yandex.ru>
2025-11-26 23:59:54 +0300

    15th sprint task accomplished

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,15 +1,36 @@
 package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.context.ApplicationContext;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ShareItTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ApplicationContext context;
 
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(context, "Контекст Spring не загружен!");
+    }
+
+    @Test
+    void testH2Connection() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            assertNotNull(connection, "Не удалось подключиться к H2!");
+            assertEquals("H2", connection.getMetaData().getDatabaseProductName());
+        }
+    }
 }
