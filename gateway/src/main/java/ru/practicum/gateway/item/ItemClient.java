5699eec573c0077b81202ef7690b1736d5df1384
AndreyAA-code <otd53@yandex.ru>
2025-12-06 15:27:28 +0300

    added booking client and controller added User client and controller

--- gateway/src/main/java/ru/practicum/gateway/item/ItemClient.java
+++ gateway/src/main/java/ru/practicum/gateway/item/ItemClient.java
@@ -1,66 +1,79 @@
 package ru.practicum.gateway.item;
 
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import ru.practicum.gateway.booking.dto.BookingCreateDto;
+import ru.practicum.gateway.booking.dto.BookingState;
+import ru.practicum.gateway.client.BaseClient;
 import ru.practicum.gateway.item.dto.comment.CommentDto;
 import ru.practicum.gateway.item.dto.item.ItemCommentsLastNextBookingDto;
 import ru.practicum.gateway.item.dto.item.ItemCreateDto;
 import ru.practicum.gateway.item.dto.item.ItemDto;
 import ru.practicum.gateway.item.dto.item.ItemUpdateDto;
 
-
 import java.util.Collection;
+import java.util.Map;
 
-@RestController
-@RequestMapping("/items")
-@RequiredArgsConstructor
-@Slf4j
-public class ItemController {
-    ItemClient itemClient;
+@Service
+public class ItemClient extends BaseClient {
+
+    private static final String URL = "/items";
+
+    @Value("${server.host}")
+    private String host;
+
+    public ItemClient() {
+        super(new RestTemplate());
+    }
+
+    public ItemClient(RestTemplate rest) {
+        super(rest);
+    }
 
     @GetMapping()
-    public ResponseEntity<Object> getItems(@RequestHeader(value = "X-Sharer-User-Id") Long userId) {
+    public Collection<ItemCommentsLastNextBookingDto> getItems(@RequestHeader(value = "X-Sharer-User-Id") Long userId) {
         log.info("getItems for user {}", userId);
-        return itemClient.getItems(userId);
+        return null;
     }
 
     @GetMapping("/{itemId}")
-    public ResponseEntity<Object>  getItemById(@PathVariable Long itemId,
+    public ItemCommentsLastNextBookingDto getItemById(@PathVariable Long itemId,
                                                       @RequestHeader(value = "X-Sharer-User-Id") Long userId) {
         log.info("getItemById {}", itemId);
-        return itemClient.getItemById(itemId, userId);
+        return null;
     }
 
     @PostMapping()
-    public ResponseEntity<Object> createItem(@Valid @RequestBody ItemCreateDto itemCreateDto,
+    public ItemDto createItem(@Valid @RequestBody ItemCreateDto itemCreateDto,
                               @RequestHeader(value = "X-Sharer-User-Id") Long userId) {
         log.info("createItem()");
-        return itemClient.createItem(itemCreateDto, userId);
+        return null;
     }
 
     @PatchMapping("/{itemId}")
-    public ResponseEntity<Object>  updateItemById(@PathVariable Long itemId,
+    public ItemDto updateItemById(@PathVariable Long itemId,
                                   @Valid @RequestBody ItemUpdateDto itemUpdateDto,
                                   @RequestHeader(value = "X-Sharer-User-Id") Long userId) {
         log.info("updateItemById()");
-        return itemClient.updateItemById(itemId, itemUpdateDto, userId);
+        return null;
     }
 
     @GetMapping ("/search")
-    public ResponseEntity<Object> searchAvailableItems(@RequestParam String text,
+    public Collection<ItemDto> searchAvailableItems(@RequestParam String text,
                                                     @RequestHeader(value = "X-Sharer-User-Id") Long userId) {
-        return itemClient.searchAvailableItems(text, userId);
+        return null;
     }
 
     @PostMapping("/{itemId}/comment")
-    public ResponseEntity<Object>  addComment(@PathVariable Long itemId,
+    public CommentDto addComment(@PathVariable Long itemId,
                                  @Valid @RequestBody CommentDto commentDto,
                                  @RequestHeader(value = "X-Sharer-User-Id") Long userId) {
         log.info("addComment for item {} by user {}: {}", itemId, userId, commentDto.getText());
-        return itemClient.addComment(itemId,commentDto, userId);
+        return null;
     }
 
 }
