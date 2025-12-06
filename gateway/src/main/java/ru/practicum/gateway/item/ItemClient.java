a5e4526d686090c88d5dd288d82ff9ef7419e6c0
AndreyAA-code <otd53@yandex.ru>
2025-12-06 15:30:58 +0300

    added booking client and controller added User client and controller

--- gateway/src/main/java/ru/practicum/gateway/item/ItemClient.java
+++ gateway/src/main/java/ru/practicum/gateway/item/ItemClient.java
@@ -1,6 +1,7 @@
 package ru.practicum.gateway.item;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -26,35 +27,28 @@ public class ItemClient extends BaseClient {
     @Value("${server.host}")
     private String host;
 
-    public ItemClient() {
-        super(new RestTemplate());
+    @Autowired
+    public ItemClient(RestTemplate restTemplate) {
+        super(restTemplate);
     }
 
-    public ItemClient(RestTemplate rest) {
-        super(rest);
-    }
-
-    @GetMapping()
     public Collection<ItemCommentsLastNextBookingDto> getItems(@RequestHeader(value = "X-Sharer-User-Id") Long userId) {
         log.info("getItems for user {}", userId);
-        return null;
+        return ;
     }
 
-    @GetMapping("/{itemId}")
     public ItemCommentsLastNextBookingDto getItemById(@PathVariable Long itemId,
                                                       @RequestHeader(value = "X-Sharer-User-Id") Long userId) {
         log.info("getItemById {}", itemId);
         return null;
     }
 
-    @PostMapping()
     public ItemDto createItem(@Valid @RequestBody ItemCreateDto itemCreateDto,
                               @RequestHeader(value = "X-Sharer-User-Id") Long userId) {
         log.info("createItem()");
         return null;
     }
 
-    @PatchMapping("/{itemId}")
     public ItemDto updateItemById(@PathVariable Long itemId,
                                   @Valid @RequestBody ItemUpdateDto itemUpdateDto,
                                   @RequestHeader(value = "X-Sharer-User-Id") Long userId) {
@@ -62,13 +56,11 @@ public class ItemClient extends BaseClient {
         return null;
     }
 
-    @GetMapping ("/search")
     public Collection<ItemDto> searchAvailableItems(@RequestParam String text,
                                                     @RequestHeader(value = "X-Sharer-User-Id") Long userId) {
         return null;
     }
 
-    @PostMapping("/{itemId}/comment")
     public CommentDto addComment(@PathVariable Long itemId,
                                  @Valid @RequestBody CommentDto commentDto,
                                  @RequestHeader(value = "X-Sharer-User-Id") Long userId) {
